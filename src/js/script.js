f5f20d429d6eb42d244123fff1fe7b59871c498a
alvesarah <sarah.alves8@hotmail.com>
2022-05-04 04:13:44 -0300

    js para troca de tema e conteudo

---
+++ src/js/script.js
@@ -0,0 +1,35 @@
+const btnProjetos = document.getElementById("btnProjetos");
+const btnHabilidades = document.getElementById("btnHabilidades");
+const projetos = document.querySelector(".displayProjetos");
+const habilidades = document.querySelector(".displayHabilidades");
+const trocaTema = document.querySelector(".trocaTema");
+const onda = document.querySelector(".onda");
+
+const themes = {
+    "dark": "light",
+    "light": "dark",
+}
+
+if(trocaTema){
+    trocaTema.addEventListener("click", event => {
+        event.preventDefault();
+        const currentTheme = document.body.dataset.tema;
+        document.body.setAttribute("data-tema", themes[currentTheme] || "light");
+        if(currentTheme === "light"){
+            onda.setAttribute("src", "./src/image/onda-dark.png");
+        } else {
+            onda.setAttribute("src", "./src/image/onda-light.png");
+        }
+    });
+}
+
+btnProjetos.addEventListener("click", () =>{
+    projetos.style.display = "flex";
+    habilidades.style.display = "none";
+});
+
+btnHabilidades.addEventListener("click", () =>{
+    habilidades.style.display = "flex";
+    habilidades.style.justifyContent = "center";
+    projetos.style.display = "none";
+});
